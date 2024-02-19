88325a24a7bb273c28e67c38cf8de2794b487dd0
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-02-18 22:48:03 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ExceptionLab
+{
+    public class Circle
+    {
+        public double radius;
+
+        public Circle()
+        {
+            radius = 0;
+        }
+
+        public void SetRadius(double r)
+        {
+            if (r > 0)
+            {
+                radius = r;
+            }
+            else
+            {
+                throw new InvalidRadiusException(r);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Radius of circle: {radius}";
+        }
+    }
+
+    public class InvalidRadiusException : Exception
+    {
+        public InvalidRadiusException()
+        {
+            Console.WriteLine("Invalid radius. Radius must be greater than zero.");
+        }
+
+        public InvalidRadiusException(double radius)
+        {
+            Console.WriteLine($"Invalid radius: {radius}. Radius must be greater than zero.");
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write($"Write a positive number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Circle circle1 = new Circle();
+                circle1.SetRadius(num1);
+                Console.WriteLine(circle1);
+
+                Console.Write($"Write a zero or negative number to get error: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Circle circle2 = new Circle();
+                circle2.SetRadius(num2); // This will throw InvalidRadiusException
+                Console.WriteLine(circle2);
+            }
+            catch (InvalidRadiusException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
